ee96531d5be382f3a09c31350fc565e4aaaee031
mayank-msj-singh <mayank.msj.singh@gmail.com>
2025-06-29 01:29:40 +0530

    schedules update (cal_get_all_schedules)

--- tools/scheduling.py
+++ tools/scheduling.py
@@ -1,7 +1,7 @@
 import requests
 
 url = "https://api.cal.com/v2/schedules"
-auth = 'cal_live_4744916169233664ad19a298a3882637'
+auth = ''
 
 def cal_get_all_schedules():
     headers = {
@@ -11,41 +11,6 @@ def cal_get_all_schedules():
 
     response = requests.request("GET", url, headers=headers)
     return(response.text)
-def cal_create_a_schedule(name, timeZone, isDefault):
-    payload = {
-        "isDefault": isDefault,
-        "name": name,
-        "timeZone": timeZone,
-        "availability": [
-            {
-                "days": ["Monday", "Tuesday"],
-                "startTime": "17:00",
-                "endTime": "19:00"
-            },
-            {
-                "days": ["Wednesday", "Thursday"],
-                "startTime": "16:00",
-                "endTime": "20:00"
-            }
-        ],
-        "overrides": [
-            {
-                "date": "2024-05-20",
-                "startTime": "18:00",
-                "endTime": "21:00"
-            }
-        ]
-    }
-
-    headers = {
-        "Authorization": "<authorization>",
-        "cal-api-version": "<cal-api-version>",
-        "Content-Type": "application/json"
-    }
-
-    response = requests.request("POST", url, json=payload, headers=headers)
-
-    return response.text
 
 if __name__ == "__main__":
     print(cal_get_all_schedules())
